492be3314c9d6f86f3e96dfdd915d6ee0db54152
Dedeyd70 <dorothyokoto@gmail.com>
2024-02-28 02:16:19 +0000

    nosql

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+ changes all topics of a school document based on the name
+"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+ """change all topics """
+ return mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
