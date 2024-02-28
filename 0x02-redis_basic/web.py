762fc981e94b77db790a34bd6ad34aa81d278e92
Dedeyd70 <Dorothyokoto@gmail.com>
2024-02-28 21:25:15 +0000

    redis basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -39,4 +39,4 @@ def get_page(url: str) -> str:
 if __name__ == "__main__":
     url= "http://slowwly.robertomurray.co.uk"
     print(get_page(url))
-     print(get_page(url))
+    print(get_page(url))
